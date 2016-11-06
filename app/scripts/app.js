97c7882b27a5df5954c525c683aa8560ded1a5c5
Matt <matthewstivali@gmail.com>
2016-11-05 20:54:29 -0400

    Moved content to main.html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,27 +25,5 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: '$ctrl'
-      })
-      .state('about', {
-        url: '/about',
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
       });
   });
-  // .config(function ($routeProvider) {
-  //   $routeProvider
-  //     .when('/', {
-  //       templateUrl: 'views/main.html',
-  //       controller: 'MainCtrl',
-  //       controllerAs: 'main'
-  //     })
-  //     .when('/about', {
-  //       templateUrl: 'views/about.html',
-  //       controller: 'AboutCtrl',
-  //       controllerAs: 'about'
-  //     })
-  //     .otherwise({
-  //       redirectTo: '/'
-  //     });
-  // });
