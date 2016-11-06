9313d5913b3994b5234a992fd28cc70f8d53b84a
Matt <matthewstivali@gmail.com>
2016-11-06 18:11:30 -0500

    Updated main controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,19 +13,15 @@ angular.module('vendingMachineApp')
 
     var self = this;
 
-    init();
-
     self.products = [];
     self.selectedProduct = {};
     self.balance = 0.0;
     self.authCodes = {awaitingAuth: 0,authorized: 1,failed: 2};
     self.creditAuthorized = 0;
 
-    function init() {
-      MainService.getProducts().then(function(products) {
-        self.products = products;
-        self.selectedProduct = self.products[0];
-      });
-    }
-
+    MainService.getProducts().then(function(products) {
+      self.products = products;
+      self.selectedProduct = self.products[0];
+    });
+    
   });
